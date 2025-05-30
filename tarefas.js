530f8a082d4cccdfa566b13056f184cef9092077
Jennifer <jennifer.k.silva7@aluno.senai.br>
2025-05-30 08:23:58 -0300

    Adiciona novas tarefas

---
+++ tarefas.js
@@ -0,0 +1,14 @@
+let tarefas = [
+  "Estudar JavaScript",
+  "Fazer exercícios de matemática",
+  "Lavar a louça",
+  "Comprar mercado",
+  "Ir à academia",
+  "Ler um livro",
+  "Fazer revisão para o teste",
+  "Limpar o quarto",
+  "Estudar para concurso",
+  "Organizar o computador",
+];
+tarefas.push(`Estudar para provas`,`Preparar lancheira`)
+console.table(`Lista de tarefas${tarefas}`);
